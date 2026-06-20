f8843e96c01f1fdae22a50a657eb0b0a9ae50d6f
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2026-06-21 01:30:27 +0700

    Remove ofd-kt-proto local composite build inclusion and add mavenLocal() fallback

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,7 @@ plugins {
 }
 rootProject.name = "ofd-proto-codec"
 
-// Если рядом есть репозиторий ofd-kt-proto, подключаем его как composite build.
-val protoRepo = file("../ofd-kt-proto")
-if (protoRepo.exists()) {
-    includeBuild("../ofd-kt-proto")
-}
+
 
 // Если рядом есть репозиторий ofd-network-client, подключаем его для тестов.
 val networkClientRepo = file("../ofd-network-client")
