529b861e1628ce7869d5cc3d9fc652e39b606fdc
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2026-06-29 12:11:48 +0700

    chore: update Package.swift checksum for v1.1.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "OfdProtoCodec",
             url: "https://github.com/texport/ofd-proto-codec/releases/download/v1.1.0/OfdProtoCodec.xcframework.zip",
-            checksum: "0f993532b0e89e96859f27f4ec8d607bc35c385bb3f85896ba76065c3b992753"
+            checksum: "e79ac6ec34e12d414550c6aaffa811ab929b22aae1e591e500e16ed6c5698c9e"
         )
     ]
 )
